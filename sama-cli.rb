4991b3c4044eac3144ba5e083373ff8b166c6f5b
samacat <eng-ops@samasource.org>
2022-11-17 12:04:08 -0500

    Brew formula update for sama-cli version v2.0.1

--- sama-cli.rb
+++ sama-cli.rb
@@ -5,20 +5,20 @@
 class SamaCli < Formula
   desc "This is the Sama CLI, a command-line tool for using the Sama Data API."
   homepage "https://github.com/Samasource/homebrew-formulas"
-  version "2.0.0"
+  version "2.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.0/sama-cli_2.0.0_darwin_arm64.tar.gz"
-      sha256 "a9799f33c72e743e7d6e569e17d76df1d7927c46ab3a50edaad626c083596494"
+      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.1/sama-cli_2.0.1_darwin_arm64.tar.gz"
+      sha256 "e5e2a795a4c604ed86781579f72767784fc78462ca042ac7fda9080447f60ee8"
 
       def install
         bin.install "sama"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.0/sama-cli_2.0.0_darwin_x86_64.tar.gz"
-      sha256 "2d50c1ca712f5aa68864876b963e7c9c335ba40c3ce8cf6d6a5989d0babe35ab"
+      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.1/sama-cli_2.0.1_darwin_x86_64.tar.gz"
+      sha256 "fdeef62229cefcee87988fe74d7c42836334f9c2d7a96c7f9d98b6a7fa8417ed"
 
       def install
         bin.install "sama"
@@ -28,16 +28,16 @@ class SamaCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.0/sama-cli_2.0.0_linux_arm64.tar.gz"
-      sha256 "c0bc34db2610fe67365e32460430ec9765acd9ce84248e0e4d334e4f548a8684"
+      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.1/sama-cli_2.0.1_linux_arm64.tar.gz"
+      sha256 "afca45a3b332c73284f65ff0b3b5dc578f38d62cac40da9f1ba0a73e3012e459"
 
       def install
         bin.install "sama"
       end
     end
     if Hardware::CPU.intel?
-      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.0/sama-cli_2.0.0_linux_x86_64.tar.gz"
-      sha256 "fcc4a109d050c8ae608f2d8ed70e4a9367e5857560b600cf460414602cd9b9e2"
+      url "http://github.com/Samasource/sama-cli/releases/download/v2.0.1/sama-cli_2.0.1_linux_x86_64.tar.gz"
+      sha256 "4d0e22e4a989eabed872880184f0d33011891019784dfd542f0b46a47b818b98"
 
       def install
         bin.install "sama"
